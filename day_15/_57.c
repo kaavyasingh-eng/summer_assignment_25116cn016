19e76c79d10cb2eb6265526b24648284ea8a6a5d
kaavya singh <263543608+kaavyasingh-eng@users.noreply.github.com>
2026-06-18 02:53:39 +0530

    Update _57.c

--- day_15/_57.c
+++ day_15/_57.c
@@ -2,17 +2,58 @@
 
 #include <stdio.h>
 
+// Function to reverse the array in-place
+void reverseArray(int arr[], int size) {
+    int start = 0;
+    int end = size - 1;
+    int temp;
+
+    // Swap elements from both ends until pointers meet
+    while (start < end) {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        // Move pointers toward the center
+        start++;
+        end--;
+    }
+}
+
+// Function to print the array elements
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
 int main() {
-    
-int arr[50];
-int i,n;
+    int size;
 
-printf("enter the no. of term of array :");
-scanf("%d",&n);
+    // Get array size from user
+    printf("Enter the size of the array: ");
+    if (scanf("%d", &size) != 1 || size <= 0) {
+        printf("Invalid array size.\n");
+        return 1;
+    }
 
-for(int i=0;i<n;i++)
+    int arr[size];
 
-{
-    printf("enter the %d term of array :",i+1);
-    scanf("%d",&arr[i]);
+    // Get array elements from user
+    printf("Enter %d elements: ", size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nOriginal array: ");
+    printArray(arr, size);
+
+    // Call the reverse function
+    reverseArray(arr, size);
+
+    printf("Reversed array: ");
+    printArray(arr, size);
+
+    return 0;
 }
